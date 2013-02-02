d75b13c06eab9d5d97eee22cca92511400014c54
Juan Jesus Izquierdo Domenech <juanjesusizquierdodomenech@gmail.com>
2013-02-02 10:37:53 +0100

    Finished!

--- TapMe/tapViewController.m
+++ TapMe/tapViewController.m
@@ -13,6 +13,10 @@
     NSInteger count;
     NSInteger seconds;
     NSTimer *timer;
+    
+    AVAudioPlayer *buttonBeep;
+    AVAudioPlayer *secondBeep;
+    AVAudioPlayer *backgroundMusic;
 }
 
 @end
@@ -24,6 +28,21 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    // IMAGES //
+    self.view.backgroundColor =
+    [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_tile.png"]];
+    
+    self.timerLabel.backgroundColor =
+    [UIColor colorWithPatternImage:[UIImage imageNamed:@"field_time.png"]];
+    
+    self.scoreLabel.backgroundColor =
+    [UIColor colorWithPatternImage:[UIImage imageNamed:@"field_score.png"]];
+    
+    // SOUNDS //
+    buttonBeep = [self setupAudioPlayerWithFile:@"ButtonTap" type:@"wav"];
+    secondBeep = [self setupAudioPlayerWithFile:@"ButtonTap" type:@"wav"];
+    backgroundMusic = [self setupAudioPlayerWithFile:@"HallOfTheMountainKing" type:@"mp3"];
     
     [self setupGame];
 }
@@ -38,28 +57,31 @@
 
 - (IBAction)buttonPressed:(UIButton *)sender
 {
-    DLog(@"Presing ca... button");
     count++;
     
     self.scoreLabel.text = [NSString stringWithFormat:@"Score: %i", count];
+    
+    [buttonBeep play];
 }
 
 #pragma mark - SetUp
 
 - (void)setupGame
 {
-    //DLog(@"Setupgame called");
     seconds = 30;
     count = 0;
     
     self.timerLabel.text = [NSString stringWithFormat:@"Time: %i", seconds];
-    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %i", count];
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score:\n%i", count];
     
     timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
-                                             target:self
-                                           selector:@selector(substractTime)
-                                           userInfo:nil
-                                            repeats:YES];
+                                              target:self
+                                            selector:@selector(substractTime)
+                                            userInfo:nil
+                                             repeats:YES];
+    [backgroundMusic setVolume:0.3];
+    [backgroundMusic play];
+
 }
 
 - (void)substractTime
@@ -67,6 +89,8 @@
     //DLog(@"Substracting time");
     seconds--;
     self.timerLabel.text = [NSString stringWithFormat:@"Time: %i", seconds];
+    
+    [secondBeep play];
     
     if (seconds == 0) {
         [timer invalidate];
@@ -81,6 +105,25 @@
         [alert show];
     }
 }
+
+#pragma mark - Audio
+
+- (AVAudioPlayer *)setupAudioPlayerWithFile:(NSString *)file type:(NSString *)type
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:file ofType:type];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    
+    NSError *err;
+    
+    AVAudioPlayer *audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&err];
+    
+    if (!audioPlayer)
+        DLog(@"Error with setupAudioPlayerWithFile:type : %@", err.localizedDescription);
+    
+    return audioPlayer;
+}
+
+#pragma mark - AlertView Delegate
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
